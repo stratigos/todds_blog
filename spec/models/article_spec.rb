8c1213735a2f1cb7b5d3a95c4363720a620a819e
Todd Morningstar <techarchist@gmail.com>
2020-05-06 12:06:56 -0700

    Write First Article Test 💯

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".published" do
+    it "selects Articles with a '#published_at' date" do
+      published_article = create(:article, published_at: 1.minute.ago)
+      unpublished_article = create(:article, published_at: nil)
+
+      result = described_class.published
+
+      expect(result).to eq([published_article])
+    end
+  end
 end
